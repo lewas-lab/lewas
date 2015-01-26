bda3417e76ae2deb0b85c0cd49377baa9622095b
Marcus Wanner <marcus@wanners.net>
2015-01-26 15:50:09 -0500

    More progress on HTTP POST datastore

--- lewas/datastores.py
+++ lewas/datastores.py
@@ -1,4 +1,4 @@
-import json
+import json, urllib, urllib2
 
 class IOPrinter():
     """A datastore for debug and testing purposes: prints output to standard stream"""
@@ -7,9 +7,12 @@ class IOPrinter():
         print(measurements)
 
 class RESTPOST():
-    """Prototype HHTP POST datastore (format experimental, no data caching)"""
+    """Prototype HTTP POST datastore (format experimental, no data caching)"""
 
     def post(self, measurements, **kwargs):
         dicts = [dict(m) for m in measurements]
         d = {m["metric"]: {a: m[a] for a in ["unit", "value"]} for m in dicts}
-        print(json.dumps(d, indent=4))
+        url = urllib2.Request('http://127.1:5050/', json.dumps(d, indent=4),
+                            {'Content-Type': 'application/json+lewas'})
+        response = urllib2.urlopen(url)
+        print(response)
