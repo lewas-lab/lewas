fea69337301cce0dce2e229d85455f1e7fb33a62
Darren Maczka <darmacz@gmail.com>
2015-02-22 08:23:54 -0500

    remove unused code

--- tests/sonde.py
+++ tests/sonde.py
@@ -10,46 +10,7 @@ import lewas.models
 import lewas.parsers
 import lewas.datastores
 
-weather_station_metrics = { 'Pa': ( 'air', 'pressure', 'hPa' ),
-                            'Rc': ( 'rain', 'accumulation', 'mm' ),
-                            'Rd': ( 'rain', 'duration', 's' ),
-                            'Ri': ( 'rain', 'intensity', 'mm/h' ),
-                            'Ta': ('air', 'temperature', 'F'),
-                            'Hc': ( 'hail', 'accumulation', 'hits/cm2' ),
-                            'Hd': ( 'hail', 'duration', 's' ),
-                            'Hi': ( 'hail', 'intensity', 'hits/cm2h'),
-                            'Sm': ('wind', 'speed', 'm/s'),
-                            'Vs': ('battery', 'voltage', 'V'),
-                            'Dm': ('wind', 'direction', 'D'),
-                            'Ua': ('air', 'humidity', '%RH')
-}
-
-unit_conversion = { 'H': 'hPa',
-                    'P': '%RH',
-                    'M': 'mm',
-                    
-                }
-
-def sonde_helper(astring):
-    """A helper should take a string representing a single measurement and
-    return a tuple or object representing that value. In this case we
-    return a Measurement object, the parser will then return a list of
-    Measurement objects
-    """
-
-    (key, value) = astring.split("=")
-    units = None
-    if key in weather_station_metrics:
-        key = weather_station_metrics[key]
-        units = key[2]
-    m = None    
-    try:
-        (value,unit) = re.search(r'([0-9]+(?:\.[0-9]+)?)([a-zA-Z#/]+)',value).groups()
-        m = lewas.models.Measurement(value, key, units, "weather station", "stroubles1")
-    except AttributeError, e:
-        print("Error parsing: {}".format(value))
-    return m
-
+# TODO: move this to parser module
 class unitParser():
     def __init__(self,typef,metric,units,instrument,site):
         self.typef = typef
@@ -75,6 +36,12 @@ class unitParser():
     
 class timeParser(unitParser):
     pass
+
+# NOTE: fields and field order are determined by the configuration of
+# the Sonde and are not necessarily constant. Eventually we should add
+# a getHeaders() function that retreives and parses the column header
+# from the Sonde. To do that, send three space characters, wait for
+# H?:, send 'H', read two lines, those should be the headers
 
 sonde_fields = [ (str, 'time', 'HHMMSS', 'time'),
                (float, 'water', 'temperature','C'),
