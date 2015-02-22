3bb3feb33c2e70c9bb418a647e9f584db3566610
Darren Maczka <darmacz@gmail.com>
2015-02-22 08:14:31 -0500

    boring serial test script
    useful for dumping serial data to file to study
    output for parser development

---
+++ tests/serial_test.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python2
+
+import sys
+sys.path.append('../')
+
+## What is clearer, a class structure
+
+import re
+import argparse
+
+if __name__ == '__main__':
+    print(sys.argv)
+    baud = sys.argv[1]
+    args = {}
+    if len(sys.argv) > 2:
+        args = dict( [ (lambda p: p.split('='))(x) for x in sys.argv[2:]])
+
+    import serial
+    datastream = serial.Serial("/dev/tty{}".format(sys.argv[1]), baud, **args) #argv[1] e.g. USB0
+
+    for line in datastream:
+        sys.stdout.write(line)
