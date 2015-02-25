041ed3ef9006b1b663246c85b707ca3a72ba1f30
Darren Maczka <darmacz@gmail.com>
2015-02-25 15:07:43 -0500

    forget generalizability right now, just setup for testing argonaut

--- tests/serial_test.py
+++ tests/serial_test.py
@@ -10,13 +10,14 @@ import argparse
 
 if __name__ == '__main__':
     print(sys.argv)
-    baud = sys.argv[1]
+    baud = sys.argv[2]
     args = {}
     if len(sys.argv) > 2:
         args = dict( [ (lambda p: p.split('='))(x) for x in sys.argv[2:]])
 
     import serial
-    datastream = serial.Serial("/dev/tty{}".format(sys.argv[1]), baud, **args) #argv[1] e.g. USB0
-
+    #datastream = serial.Serial("/dev/tty{}".format(sys.argv[1]), baud, **args) #argv[1] e.g. USB0
+    datastream = serial.Serial("/dev/tty{}".format(sys.argv[1]), baud, stopbits=serial.STOPBITS_ONE) #argv[1] e.g. USB0
+    
     for line in datastream:
         sys.stdout.write(line)
