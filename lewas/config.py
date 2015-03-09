bed0099824d1420e8190cc421ce558f9dc11b1b5
Marcus Wanner <marcus@wanners.net>
2015-03-09 18:23:44 -0400

    Very early SSL auth WIP

--- lewas/config.py
+++ lewas/config.py
@@ -5,7 +5,9 @@ class Config():
         c = ConfigParser.RawConfigParser()
         c.read(os.path.abspath(config))
         d = {i[0]: i[1] for i in c.items("leapi")}
-        for attr in ['host','password',('storage','../requests'),('endpoint','/observations')]:
+        for attr in ['host','password','sslkey','sslcrt',
+                ('storage','../requests'),
+                ('endpoint','/observations')]:
             if hasattr(attr, '__iter__'):
                 setattr(self, attr[0], d.get(attr[0], attr[1]))
             else:
