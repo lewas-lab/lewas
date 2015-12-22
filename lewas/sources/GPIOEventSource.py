4ea372f4f6a859eb547b2bafd5edb69d0d331e1b
Darren Maczka <dmaczka@vt.edu>
2015-12-22 16:57:38 -0500

    reindent

--- lewas/sources/GPIOEventSource.py
+++ lewas/sources/GPIOEventSource.py
@@ -7,8 +7,8 @@ import rpi
 
 def first_inpin(pins):
     for pin in pins:
-	if pin.direction == GPIO.IN:
-		return pin
+        if pin.direction == GPIO.IN:
+            return pin
     raise NoInputPin()
 
 def GPIOEventSource(pin_setup, **kwargs):
@@ -25,13 +25,13 @@ def GPIOEventSource(pin_setup, **kwargs):
         logging.debug('setting pin number {} to {} with {}'.format(p.channel, p.direction, p.kwargs))
         if p.direction == GPIO.OUT and p.value is not None:
             logging.debug('setting pin number {} output to {}'.format(p.channel, p.value))
-		GPIO.output(p.channel, p.value)
+            GPIO.output(p.channel, p.value)
 
     input_pin = first_inpin(pin_setup)
     with GPIOEventAccumulator(input_pin.channel, mode=detect_method, direction=direction, bouncetime=bouncetime) as events:
-    	while events.is_alive:
-        	yield events.count
-        	sleep(interval)
+        while events.is_alive:
+            yield events.count
+            sleep(interval)
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
